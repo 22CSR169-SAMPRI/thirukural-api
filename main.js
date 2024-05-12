fea6a702bab9c7c69eb3f3028ef3577333a6f6ae
sampritha-a <samprithaa.22cse@kongu.edu>
2024-05-12 16:09:50 +0530

    feat: app structure

---
+++ main.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app=express()
+const PORT=6969;
+app.get('/',(req, res)=>{
+    res.json({ msg: " heollo"});
+})
+
+app.listen(PORT,(req,res)=>{
+    console.log(`server in http://localhost:${PORT}`);
+
+})
