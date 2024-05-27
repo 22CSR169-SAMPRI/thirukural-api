75420b66eacc2104f1cfaa89ba8b811bc4e97d59
sampritha-a <samprithaa.22cse@kongu.edu>
2024-05-27 21:30:29 +0530

    feat: middleware

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 import express from "express";
+import kuralRoutes from "./routes/kural.route" 
 const app=express()
 const PORT=6969;
+
+
 app.get('/',(req, res)=>{
     res.json({ msg: " heollo"});
 })
-
+app.use('/kural',kuralRoutes);
 app.listen(PORT,(req,res)=>{
     console.log(`server in http://localhost:${PORT}`);
 
 })
-app.get("/kural",()=>{});
-app.post("/kural",()=>{});
-app.put("/kural/:id",()=>{});
-app.delete("/kural/:id",()=>{});
---
+++ routes/kural.route.js
@@ -0,0 +1,7 @@
+import express from 'express'
+const router=express.Router()
+router.get("/kural",()=>{});
+router.post("/kural",()=>{});
+router.put("/kural/:id",()=>{});
+router.delete("/kural/:id",()=>{});
+export default router;
