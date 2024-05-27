bddc4accbd3cb7ef57ea103bfe78912f84cbe874
sampritha-a <samprithaa.22cse@kongu.edu>
2024-05-27 21:18:39 +0530

    route structure

--- main.js
+++ main.js
@@ -9,3 +9,7 @@ app.listen(PORT,(req,res)=>{
     console.log(`server in http://localhost:${PORT}`);
 
 })
+app.get("/kural",()=>{});
+app.post("/kural",()=>{});
+app.put("/kural/:id",()=>{});
+app.delete("/kural/:id",()=>{});
